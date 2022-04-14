8c28ab0d744f75e9102e041acad4fb21e27b29f4
tehya-laughlin <tehyanlaugh.shop@gmail.com>
2022-04-14 10:37:19 -0600

    assignment 13

--- js/assignment13.js
+++ js/assignment13.js
@@ -12,21 +12,34 @@ const insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
 const insertY = ["the soup kitchen", "Disneyland", "the White House"];
 const insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
 
-randomize.addEventListener('click', result);
 
 function result() {
+  let newStory = storyText;
+
+  let xItem = randomValueFromArray(insertX);
+  let yItem = randomValueFromArray(insertY);
+  let zItem = randomValueFromArray(insertZ);
+  newStory = newStory.replace(/:insertx:/g,xItem);
+  newStory = newStory.replace(/:inserty:/g,yItem);
+  newStory = newStory.replace(/:insertz:/g,zItem);
+
 
   if(customName.value !== '') {
     const name = customName.value;
+    newStory = newStory.replace(/Bob/g,name);
 
   }
 
-  if(document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature =  Math.round(94);
+  if(document.getElementById('uk').checked) {
+    const weight = Math.round(300/14);
+    const temperature =  Math.round((94-32) * (5/9));
+    newStory = newStory.replace(/94 fahrenheit/g, temperature+' centigrade');
+    newStory = newStory.replace(/300 pounds/g, weight+' stone');
 
   }
 
-  story.textContent = ;
+  story.textContent = newStory;
   story.style.visibility = 'visible';
 }
+
+randomize.addEventListener('click', result);
